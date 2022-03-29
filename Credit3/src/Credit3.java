84d4fc756fd9df757cdedae30e90459deaeed6bb
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-03-29 15:15:09 -0600

    Money

--- Credit3/src/Credit3.java
+++ Credit3/src/Credit3.java
@@ -2,7 +2,11 @@
 public class Credit3 {
 
 	public static void main(String[] args) {
-		// 12333
+		System.out.println("From: Mr. Wilson");
+		System.out.println("Address: Acer Computer, Lab 314");
+		System.out.println("Date: November2, 2020");
+		System.out.println("To: Enter Your First Name and Last Name");
+		System.out.println("Message: Help! I'm trapped inside a computer!");
 
 	}
 
