bd50e98af721dd770cad62489d0186d7de37e759
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-03-29 15:29:31 -0600

    Money

--- Credit3/src/Credit3.java
+++ Credit3/src/Credit3.java
@@ -2,11 +2,7 @@
 public class Credit3 {
 
 	public static void main(String[] args) {
-		System.out.println("From: Mr. Wilson");
-		System.out.println("Address: Acer Computer, Lab 314");
-		System.out.println("Date: November2, 2020");
-		System.out.println("To: Enter Your First Name and Last Name");
-		System.out.println("Message: Help! I'm trapped inside a computer!");
+		System.out.println("Hello World");
 
 	}
 
---
+++ Credit3/src/frommetoyou.java
@@ -0,0 +1,13 @@
+
+public class 
+frommetoyou {
+	public static void main(String[] args) {
+		System.out.println("From: Mr. Wilson");
+		System.out.println("Address: Acer Computer, Lab 314");
+		System.out.println("Date: November2, 2020");
+		System.out.println("To: Enter Your First Name and Last Name");
+		System.out.println("Message: Help! I'm trapped inside a computer!");
+
+	}
+
+}
