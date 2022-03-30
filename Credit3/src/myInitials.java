43df9d302b1b3417a2b98a7d1df498bd491de4c5
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-03-30 13:51:19 -0600

    Money

--- Credit3/src/myInitials.java
+++ Credit3/src/myInitials.java
@@ -5,7 +5,13 @@ myInitials
 	public static void main(String[] args)
 	{ System.out.println("Money Mann 3/30/2022 \n\n");
 	
-		System.out.println("*Money Mann*");
+		System.out.println("Enter your first name: ");
+		System.out.println("Enter your last name: ");
+		
+		String first = "\n\nM.";
+		String last = "M.";
+		
+		System.out.println(first + last);
 	}
 	
 
