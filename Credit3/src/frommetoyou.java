46d957b0aa3a27e26df49d804a97e7c94fa6b820
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-03-30 12:26:02 -0600

    Money

--- Credit3/src/frommetoyou.java
+++ Credit3/src/frommetoyou.java
@@ -5,7 +5,7 @@ frommetoyou {
 		System.out.println("From: Mr. Wilson");
 		System.out.println("Address: Acer Computer, Lab 314");
 		System.out.println("Date: November2, 2020");
-		System.out.println("To: Enter Your First Name and Last Name");
+		System.out.println("To: Money Mann");
 		System.out.println("Message: Help! I'm trapped inside a computer!");
 
 	}
