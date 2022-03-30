d62ddd42de30c8494a540a83d6c7dbe2d2554562
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-03-30 13:04:47 -0600

    updating FromMeToYou

--- Credit3/src/frommetoyou.java
+++ Credit3/src/frommetoyou.java
@@ -1,11 +1,13 @@
 
 public class 
-frommetoyou {
-	public static void main(String[] args) {
+frommetoyou 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("From: Mr. Wilson");
 		System.out.println("Address: Acer Computer, Lab 314");
-		System.out.println("Date: November2, 2020");
-		System.out.println("To: Money Mann");
+		System.out.println("Date: November2, 2020\n\n");
+		System.out.println("To: Money Mann\n\n");
 		System.out.println("Message: Help! I'm trapped inside a computer!");
 
 	}
