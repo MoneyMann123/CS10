00973fb6d4c60cb73e20a4ae25d7751fbd29aae7
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-01 11:32:32 -0600

    Money

---
+++ Credit3/src/PizzaCost.java
@@ -0,0 +1,22 @@
+
+public class 
+PizzaCost 
+{
+	public static void main(String[] args)
+	{ System.out.println("Money Mann 4/1/2022 \n\n");
+		
+		double Laborcost = 0.75;
+		int rentCost = 1;
+		double materialCost = 0.05 * 10 * 10;
+		
+		double totalCost = materialCost + Laborcost + rentCost;
+		
+		System.out.println("The total cost of making the pizza is totalcost");
+		
+
+
+
+
+
+	}
+}
