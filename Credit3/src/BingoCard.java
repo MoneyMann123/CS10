081a8308eae50aaf36d7064da69858ed7196f0f4
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-01 11:01:14 -0600

    Money

---
+++ Credit3/src/BingoCard.java
@@ -0,0 +1,17 @@
+
+public class 
+BingoCard 
+{
+	public static void main(String[] args)
+	{ System.out.println("Money Mann 4/1/2022 \n\n");
+		
+		System.out.println("B \t I \t N \t G \t O");
+		System.out.println("8 \t 16 \t 45 \t 55 \t 74");
+		System.out.println("1 \t 25 \t 31 \t 51 \t 62");
+		System.out.println("12 \t 29 \t Free! \t 49 \t 65");
+		System.out.println("5 \t 21 \t 42 \t 53 \t 71");
+		System.out.println("6 \t 18 \t 36 \t 47 \t 68");
+
+
+	}
+}
