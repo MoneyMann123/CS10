6f9d2d79239dafde2f4d63be7e39039c658f01f1
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-04 13:44:15 -0600

    Money

---
+++ Credit3/src/Assignment5.java
@@ -0,0 +1,16 @@
+
+public class 
+Assignment5 
+{
+	public static void main(String[] args)
+	{System.out.println("Money Mann 4/4/2022 \n");
+		
+		System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + "145 \n");
+		System.out.println("(179 +21 +10) / 7 + 181 = " + "211 \n");
+		System.out.println("10389 * 56 * 11 + 2246 = " + "6401870");
+		
+
+		
+	}
+
+}
