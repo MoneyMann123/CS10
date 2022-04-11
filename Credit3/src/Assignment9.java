88704640dcd2bb92cbeb6cce5b5d425830b7f867
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-11 13:53:23 -0600

    Money

---
+++ Credit3/src/Assignment9.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/*
+
+Program: Assignment9.java          Date: 4/11/2022
+
+
+Author: Please enter your first and last name here 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class Assignment9 
+{ public static void main(String[] args)
+	{System.out.println("Money Mann 4/11/2022 \n\n");
+	
+	Scanner userinput = new Scanner(System.in);
+	
+	
+	
+	
+	
+	}
+
+}
+/* Screen Dump
+
+
+ */
