de4a52a7a06f3443560fcc640da981024987096e
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-12 15:29:28 -0600

    Money

--- Credit3/src/Exercises/Exercise1.java
+++ Credit3/src/Exercises/Exercise1.java
@@ -3,7 +3,9 @@ package Exercises;
 public class Exercise1 
 {	
 	public static void main(String[] args)
-	{
+	{	int x = 5;
+		int y = 10;
+		System.out.println(Math.max(x, y));
 		
 	}
 
--- Credit3/src/Exercises/Exercise2.java
+++ Credit3/src/Exercises/Exercise2.java
@@ -1,5 +1,11 @@
 package Exercises;
 
-public class Exercise2 {
+public class Exercise2 
+{	
+	public static void main(String[] args)
+	{	int x = 16;
+		System.out.println(Math.sqrt(x));
+		
+	}
 
 }
