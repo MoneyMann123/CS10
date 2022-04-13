d34b6b8371bc243357371c3034e714693bd7850c
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-13 13:56:05 -0600

    Money

--- Credit3/src/Exercises/Exzercise18.java
+++ Credit3/src/Exercises/Exzercise18.java
@@ -1,5 +1,12 @@
 package Exercises;
 
-public class Exzercise18 {
+public class Exzercise18
+{	
+	public static void main(String[] args)
+	{	String firstName = "John ";
+		String lastName = "Doe";
+		System.out.println(firstName.concat(lastName));
+		
+	}
 
 }
--- Credit3/src/Exercises/Exzercise19.java
+++ Credit3/src/Exercises/Exzercise19.java
@@ -1,5 +1,11 @@
 package Exercises;
 
-public class Exzercise19 {
+public class Exzercise19
+{	
+	public static void main(String[] args)
+	{	String txt = "Hello Everybody";
+		System.out.println(txt.indexOf("e"));
+		
+	}
 
 }
