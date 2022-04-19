c72c53856b49b769514af22a13f380f40e5ed120
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-19 14:18:32 -0600

    Money

---
+++ Credit5/src/Assignments/Assignment11.java
@@ -0,0 +1,12 @@
+package Assignments;
+import java.util.Scanner;
+
+public class Assignment11
+{	
+	public static void main(String[] args)
+	{	System.out.println("Money Mann 4/14/2022 \n\n");
+	
+		Scanner userinput = new Scanner(System.in);
+	
+	}
+}
