f8588383e4638894558cde4db9a7ded566cabb10
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-25 12:31:20 -0600

    money

--- Credit4/src/Assignments/Assignment7.java
+++ Credit4/src/Assignments/Assignment7.java
@@ -22,15 +22,14 @@ public class Assignment7
 	System.out.println("Enter the change in cents: ");//ask the user to enter the change in cents
 	int change = userinput.nextInt();//assign the user input of change in cents to a variable called change
 	
-	int quarters = change / 25;
-	int dimes = (change % 25) / 10;
-	int nickels = ((change % 25) %10) /5;
+	int quarters = change / 25;//record quarters
+	int dimes = (change % 25) / 10;//record dimes
+	int nickels = ((change % 25) %10) /5;//record nickels
 	
-	System.out.println("The change you provided is: " + change + "\n");
-	
-	System.out.println("Quarters: " + quarters);
-	System.out.println("Dimes: " + dimes);
-	System.out.println("Nickels: " + nickels);
+	System.out.println("The change you provided is: " + change + "\n");//inform the user of the change they provided
+	System.out.println("Quarters: " + quarters);//input number of quarters
+	System.out.println("Dimes: " + dimes);//input number of dimes
+	System.out.println("Nickels: " + nickels);//input number of nickels
 	
 	}
 
--- Credit4/src/Assignments/Assignment8twodigit.java
+++ Credit4/src/Assignments/Assignment8twodigit.java
@@ -14,17 +14,17 @@ Course: Computer Science 10
 */
 public class Assignment8twodigit 
 { public static void main(String[] args)
-	{System.out.println("Money Mann 4/11/2022 \n\n");
+	{System.out.println("Money Mann 4/11/2022 \n\n");//print first and last name with date
 	
-	Scanner userinput = new Scanner(System.in);
+	Scanner userinput = new Scanner(System.in);//create a scanner object
 	
-	System.out.println("Enter a two digit number: ");
-	int number = userinput.nextInt();
+	System.out.println("Enter a two digit number: ");//ask user to input a 2 digit number
+	int number = userinput.nextInt();//assign the user input to variable called number
 	
-	System.out.println("Your two digit number is: " + number + "\n" );
+	System.out.println("Your two digit number is: " + number + "\n" );//inform the user of their 2 digit number
 	
-	System.out.println("The tens-place digit is: " + number/10 );
-	System.out.println("The ones-place digit is: " + number % 10);
+	System.out.println("The tens-place digit is: " + number/10 );//calculate and print the tens place digit in the 2 digit number
+	System.out.println("The ones-place digit is: " + number % 10);//calculate and print the ones place digit in the 2 digit number
 	
 	
 	
