f696dbd06dd22652a5fc58e40eea0fdd1e5274da
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-25 12:35:08 -0600

    Money

--- Credit4/src/Assignments/Assignment9.java
+++ Credit4/src/Assignments/Assignment9.java
@@ -14,27 +14,27 @@ Course: Computer Science 10
 */
 public class Assignment9 
 { public static void main(String[] args)
-	{System.out.println("Money Mann 4/11/2022 \n\n");
+	{System.out.println("Money Mann 4/11/2022 \n\n");//print first and last name with date
 	
-	Scanner userinput = new Scanner(System.in);
+	Scanner userinput = new Scanner(System.in);//create a scanner object
 	
-	System.out.println("Enter an integer: ");
-	int number = userinput.nextInt();
+	System.out.println("Enter an integer: ");//ask user to input a integer
+	int number = userinput.nextInt();//assign the user input to variable called number
 	
-	System.out.println("Enter a second integer: ");
-	int othernumber = userinput.nextInt();
+	System.out.println("Enter a second integer: ");//ask the user to input another integer
+	int othernumber = userinput.nextInt();//assign the user input to variable called othernumber
 	
-	int firstanswer = number / othernumber;
-	int secondanswer = number % othernumber;
+	int firstanswer = number / othernumber; // record firstanswer
+	int secondanswer = number % othernumber;//record secondanswer
 	
-	int thirdanswer = othernumber / number;
-	int fourthanswer = othernumber % number;
+	int thirdanswer = othernumber / number;//record thirdanswer
+	int fourthanswer = othernumber % number;//record fourthanswer
 	
-	System.out.println("\n" + number + " / " + othernumber + " = " + firstanswer);
-	System.out.println("\n" + number + " % " + othernumber + " = " + secondanswer);
+	System.out.println("\n" + number + " / " + othernumber + " = " + firstanswer);//print answer to user
+	System.out.println("\n" + number + " % " + othernumber + " = " + secondanswer);//print answer to user
 	
-	System.out.println("\n" + othernumber + " / " + number + " = " + thirdanswer);
-	System.out.println("\n" + othernumber + " % " + number + " = " + fourthanswer);
+	System.out.println("\n" + othernumber + " / " + number + " = " + thirdanswer);//print answer to user
+	System.out.println("\n" + othernumber + " % " + number + " = " + fourthanswer);//print answer to user
 	
 	
 	}
