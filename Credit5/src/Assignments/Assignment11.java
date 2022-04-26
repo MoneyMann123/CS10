67fc23c5024044b2f95703cf74363ab190144ca7
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-26 14:44:01 -0600

    money

--- Credit5/src/Assignments/Assignment11.java
+++ Credit5/src/Assignments/Assignment11.java
@@ -16,7 +16,7 @@ Course: Computer Science 10
 public class Assignment11
 {	
 	public static void main(String[] args)
-	{	System.out.println("Money Mann 4/14/2022 \n\n");
+	{	System.out.println("Money Mann 4/26/2022 \n\n");
 	
 		Scanner userinput = new Scanner(System.in);
 		
@@ -39,7 +39,7 @@ public class Assignment11
 			min = digit1;
 			
 			System.out.println("The largest of the three digits is: " + digit3);
-			System.out.println("The smalles of the three sigits is: " + digit1);
+			System.out.println("The smallest of the three sigits is: " + digit1);
 		}
 		else if(digit1 > digit2 && digit2 > digit3) 
 		{
@@ -48,9 +48,65 @@ public class Assignment11
 			
 			System.out.println("The largest of the three digits is: " + digit1);
 			System.out.println("The smallest of the three digits is :" + digit3);
-			else if()
+		} 
+		else if(digit2 > digit1 && digit1 > digit3) 
+		{
+			max = digit2;
+			min = digit3;
+			
+			System.out.println("The largest of the three digits is: " + digit2);
+			System.out.println("The smallest of the three digits is: " + digit3);
+		} 
+		else if(digit3 > digit2 && digit2 > digit1) 
+		{
+			max = digit3;
+			min = digit1;
 		}
+		else if(digit3 > digit1 && digit1 > digit2) 
+		{
+			max = digit3;
+			min = digit2;
+			
+			System.out.println("The largest of the three digits is: " + digit3);
+			System.out.println("The smallest of the three digits is: " + digit2);
+		}
+		else 
+		{
+			System.out.println("WRONG INPUT, CANNOT HAVE SAME DIGITS, TRY AGAIN!");
+		}
+		System.out.println("\n");
 		
+		int sum = digit1 + digit2 + digit3;
+		
+		System.out.println("The sum of the three digits is: " + sum );
+		
+		int product = digit1 * digit2 * digit3;
+		
+		System.out.println("The product of the three digits is: " + product);
+		
+		int average = (digit1 + digit2 + digit3) / 3;
+		
+		System.out.println("The average of the three digits is: " + average);
 		
 	}
 }
+/* Screen Dump
+
+Money Mann 4/26/2022 
+
+
+Please enter a three digit number: 
+123
+your first digit is: 1
+your second digit is: 2
+your third digit is: 3
+
+The largest of the three digits is: 3
+The smallest of the three sigits is: 1
+
+
+The sum of the three digits is: 6
+The product of the three digits is: 6
+The average of the three digits is: 2
+
+ */
