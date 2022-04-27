a032becfb139eee3df4d0518cfe11429c857a3ff
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-27 13:48:29 -0600

    money

--- Credit5/src/Assignments/Assignment13.java
+++ Credit5/src/Assignments/Assignment13.java
@@ -17,6 +17,28 @@ public class Assignment13
 {
 	public static void main(String[] args) 
 	{
+		System.out.println("Money Mann 4/27/2022 \n\n");//print first and last name with date
+		
+		Scanner userinput = new Scanner(System.in);//create scanner object
+		
+		System.out.println("Enter the number of copies to be printed: ");
+		int num = userinput.nextInt();//assign user input to variable num
+		
+		if(num < 100) 
+		{
+			System.out.println("Price per copy is: $0.30" + "\n");
+			System.out.println("Total cost is: $" + num * 0.30);
+		}
+		else if(num > 100 && num < 499) 
+		{
+			System.out.println("Price per copy is: $0.28" + "\n");
+			System.out.println("Total cost is: $" + num * 0.28);
+		}
+		else if(num > 500 && num < 749) 
+		{
+			System.out.println("Price per copy is: $0.27" + "\n");
+			System.out.println("Total cost is: $" + num * 0.27);
+		}
 		
 		
 		
