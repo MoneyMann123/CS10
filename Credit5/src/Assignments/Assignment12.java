c7a1176d63bc8ed28e8609458053447366e030dc
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-27 12:11:01 -0600

    Monney

---
+++ Credit5/src/Assignments/Assignment12.java
@@ -0,0 +1,18 @@
+package Assignments;
+import java.util.Scanner;
+
+/*
+
+Program: Assignment12.java          Date: 4/27/2022
+
+
+Author: Money Mann  
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+public class Assignment12 {
+	
+}
