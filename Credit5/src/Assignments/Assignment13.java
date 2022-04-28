5198fe9d14c8d12fdc43497fa35db5a54f0f0a8a
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-28 12:14:28 -0600

    money

--- Credit5/src/Assignments/Assignment13.java
+++ Credit5/src/Assignments/Assignment13.java
@@ -21,46 +21,47 @@ public class Assignment13
 		
 		Scanner userinput = new Scanner(System.in);//create scanner object
 		
-		System.out.println("Enter the number of copies to be printed: ");
+		System.out.println("Enter the number of copies to be printed: ");//ask user to input number of copies
 		int num = userinput.nextInt();//assign user input to variable num
 		
-		if(num < 100) 
+		if(num < 100) //if statement for if the user input is less than 100
 		{
-			System.out.println("Price per copy is: $0.30" + "\n");
-			System.out.println("Total cost is: $" + num * 0.30);
+			System.out.println("Price per copy is: $0.30" + "\n");//print price of each copy
+			System.out.println("Total cost is: $" + num * 0.30);//print total price for all copies
 		}
-		else if(num > 100 && num < 499) 
+		else if(num > 100 && num < 499) //else if statement for if the user input is less than 499 and greater than 100
 		{
-			System.out.println("Price per copy is: $0.28" + "\n");
-			System.out.println("Total cost is: $" + num * 0.28);
+			System.out.println("Price per copy is: $0.28" + "\n");//print price for each copy 
+			System.out.println("Total cost is: $" + num * 0.28);//print total price for all copies
 		}
-		else if(num > 500 && num < 749) 
+		else if(num > 500 && num < 749) //else if statement for if the user input is greater than 500 and less than 749
 		{
-			System.out.println("Price per copy is: $0.27" + "\n");
-			System.out.println("Total cost is: $" + num * 0.27);
+			System.out.println("Price per copy is: $0.27" + "\n");//print price for each copy
+			System.out.println("Total cost is: $" + num * 0.27);//print total price for all copies
 		}
-		else if (num > 750 && num < 1000) 
+		else if (num > 750 && num < 1000)//else if statement for if the user input is greater than 750 and less than 1000
 		{
-			System.out.println("Price per copy is: $0.26" + "\n");
-			System.out.println("Total cost is: $" + num * 0.26);
+			System.out.println("Price per copy is: $0.26" + "\n");//print price for each copy
+			System.out.println("Total cost is: $" + num * 0.26);//print total price for all copies
 		}
-		else if(num > 1000)
+		else if(num > 1000)//else if statement for if the user input is greater than 1000
 		{
-			System.out.println("Price per copy is: $0.25" + "\n");
-			System.out.println("Total price is: $" + num * 0.25);
+			System.out.println("Price per copy is: $0.25" + "\n");//print price for each copy
+			System.out.println("Total price is: $" + num * 0.25);//print total cost for all copies
 			
 		}
-		else 
-		{
-			System.out.println("ERROR, TRY DIFFERENT NUMBER");
-		}
-		
-		
-		
-		
-		
-		
-		
-		
 	}
 }
+
+/* Screen Dump
+
+Money Mann 4/27/2022 
+
+
+Enter the number of copies to be printed: 
+1234
+Price per copy is: $0.25
+
+Total price is: $308.5
+
+*/
