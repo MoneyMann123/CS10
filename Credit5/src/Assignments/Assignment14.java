c70a83daa889c9c41b15e4b9fa47fc01a3db1767
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-28 14:14:04 -0600

    money

--- Credit5/src/Assignments/Assignment14.java
+++ Credit5/src/Assignments/Assignment14.java
@@ -23,24 +23,28 @@ public class Assignment14
 		
 		System.out.println("Enter your age: ");//ask user to input their age
 		int age = userinput.nextInt();//assign user age to int age
-	
 		
+		System.out.println("Your age is: " + age + "\n");
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		if(age > 18) 
+		{
+			System.out.println("Adult");
+		}
+		else if(age > 12 && age < 18) 
+		{
+			System.out.println("Teen");
+		}
+		else if(age <= 12 && age > 10) 
+		{
+			System.out.println("Preteen");
+		}
+		else if(age <= 10 && age > 5) 
+		{
+			System.out.println("Child");
+		}
+		else if(age <= 5 && age > 0) 
+		{
+			System.out.println("Toddler");
+		}
 	}
 }
