0f65663b4d87bbad88dcf1ae48bbef6371aaca1f
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-28 15:13:01 -0600

    money

--- Credit5/src/Assignments/Assignment15.java
+++ Credit5/src/Assignments/Assignment15.java
@@ -1,5 +1,39 @@
 package Assignments;
+import java.util.Scanner;
 
-public class Assignment15 {
+/*
 
+Program: Assignment15.java          Date: 4/28/2022
+
+
+Author: Money Mann  
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+public class Assignment15
+{
+	public static void main (String[] args) 
+	{
+		System.out.println("Money Mann 4/27/2022 \n\n");//print first and last name with date
+		
+		Scanner userinput = new Scanner(System.in);//create scanner object
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
