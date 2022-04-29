ac85d2b7693e4ce1d09c11d2e3ff1666510b5036
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-29 11:08:02 -0600

    money

--- Credit5/src/Assignments/Assignment15.java
+++ Credit5/src/Assignments/Assignment15.java
@@ -21,6 +21,55 @@ public class Assignment15
 		
 		Scanner userinput = new Scanner(System.in);//create scanner object
 		
+		System.out.println("Enter a number from 1 to 4: ");
+		int operator = userinput.nextInt();
+		
+		int firstnum = (int) (1 + (Math.random() * 10));
+		int secondnum = (int) (1 + (Math.random() * 10));
+		
+		switch (operator) 
+		{
+		case 1: 
+			System.out.println("What is " + firstnum + " * " + secondnum + "?: ");
+			int answer1 = userinput.nextInt();
+			
+			if(answer1 == firstnum * secondnum) 
+			{
+				System.out.println("You got it right! Congratulation!!");
+			}
+			break;
+			
+		case 2:
+			System.out.println("What is " + firstnum + " + " + secondnum + "?: ");
+			int answer2 = userinput.nextInt();
+			
+			if(answer2 == firstnum + secondnum) 
+			{
+				System.out.println("You got it right! Congratulation!!");
+			}
+			break;
+			
+		case 3:
+			System.out.println("What is " + firstnum + " - " + secondnum + "?: ");
+			int answer3 = userinput.nextInt();
+			
+			if(answer3 == firstnum - secondnum) 
+			{
+				System.out.println("You got it right! Congratulation!!");
+			}
+			break;
+			
+		case 4:
+			System.out.println("What is " + firstnum + " / " + secondnum + "?: ");
+			int answer4 = userinput.nextInt();
+			
+			if(answer4 == firstnum / secondnum) 
+			{
+				System.out.println("You got it right! Congratulation!!");
+			}
+			break;
+			
+		}
 		
 		
 		
