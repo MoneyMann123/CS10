815f0456ec73ec9092e0576331224d89e604bebc
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-04-29 11:32:34 -0600

    money

--- Credit5/src/Assignments/Assignment15.java
+++ Credit5/src/Assignments/Assignment15.java
@@ -21,53 +21,85 @@ public class Assignment15
 		
 		Scanner userinput = new Scanner(System.in);//create scanner object
 		
-		System.out.println("Enter a number from 1 to 4: ");
-		int operator = userinput.nextInt();
+		System.out.println("Enter a number from 1 to 4: ");//ask user to enter a number from 1 - 4 which will determine the operator
+		int operator = userinput.nextInt();//create an operator variable for the number the user inputs
 		
-		int firstnum = (int) (1 + (Math.random() * 10));
-		int secondnum = (int) (1 + (Math.random() * 10));
+		int firstnum = (int) (1 + (Math.random() * 10));//find first random number
+		int secondnum = (int) (1 + (Math.random() * 10));//find second random number
 		
-		switch (operator) 
+		switch (operator)// create switch function depending on the operator the user got
 		{
-		case 1: 
-			System.out.println("What is " + firstnum + " * " + secondnum + "?: ");
-			int answer1 = userinput.nextInt();
+		case 1: //use this case if user entered 1
+			System.out.println("What is " + firstnum + " * " + secondnum + "?: ");//multiply first random number to second random number
+			int answer1 = userinput.nextInt();//assign user answer to variable int answer1
 			
-			if(answer1 == firstnum * secondnum) 
+			if(answer1 == firstnum * secondnum)//check if the user got the question right by stating if statement
 			{
-				System.out.println("You got it right! Congratulation!!");
+				System.out.println("You got it right! Congratulation!!");//tell user if they are right
 			}
-			break;
+			else if(answer1 < firstnum * secondnum) //state if statement for if the user inputed an answer less than the correct answer
+			{
+				System.out.println("Try Again");//ask user to try again
+			}
+			else if(answer1 > firstnum * secondnum) //state if statement for if the user inputed an answer greater than the correct answer
+			{
+				System.out.println("Try Again");//ask user to try again
+			}
+			break;//break out of case
 			
 		case 2:
-			System.out.println("What is " + firstnum + " + " + secondnum + "?: ");
-			int answer2 = userinput.nextInt();
+			System.out.println("What is " + firstnum + " + " + secondnum + "?: ");//add up first random number and second random number
+			int answer2 = userinput.nextInt();//assign user answer to variable int answer2
 			
-			if(answer2 == firstnum + secondnum) 
+			if(answer2 == firstnum + secondnum) ////check if the user got the question right by stating if statement
 			{
-				System.out.println("You got it right! Congratulation!!");
+				System.out.println("You got it right! Congratulation!!");//tell user if they are right
 			}
-			break;
+			else if(answer2 < firstnum + secondnum) //state if statement for if the user inputed an answer less than the correct answer
+			{
+				System.out.println("Try Again");//ask user to try again
+			}
+			else if(answer2 > firstnum + secondnum) //state if statement for if the user inputed an answer greater than the correct answer
+			{
+				System.out.println("Try Again");//ask user to try again
+			}
+			break;//break out of case
 			
 		case 3:
-			System.out.println("What is " + firstnum + " - " + secondnum + "?: ");
-			int answer3 = userinput.nextInt();
+			System.out.println("What is " + firstnum + " - " + secondnum + "?: ");//Subtract second random number from first random number 
+			int answer3 = userinput.nextInt();//assign user answer to variable int answer3
 			
-			if(answer3 == firstnum - secondnum) 
+			if(answer3 == firstnum - secondnum) //check if the user got the question right by stating if statement
 			{
-				System.out.println("You got it right! Congratulation!!");
+				System.out.println("You got it right! Congratulation!!");//tell user if they are right
 			}
-			break;
+			else if(answer3 < firstnum - secondnum) //state if statement for if the user inputed an answer less than the correct answer
+			{
+				System.out.println("Try Again");//ask user to try again
+			}
+			else if(answer3 > firstnum - secondnum) //state if statement for if the user inputed an answer greater than the correct answer
+			{
+				System.out.println("Try Again");//ask user to try again
+			}
+			break;//break out of case
 			
 		case 4:
-			System.out.println("What is " + firstnum + " / " + secondnum + "?: ");
-			int answer4 = userinput.nextInt();
+			System.out.println("What is " + firstnum + " / " + secondnum + "?: ");//
+			int answer4 = userinput.nextInt();//assign user answer to variable int answer4
 			
-			if(answer4 == firstnum / secondnum) 
+			if(answer4 == firstnum / secondnum)//check if the user got the question right by stating if statement
 			{
-				System.out.println("You got it right! Congratulation!!");
+				System.out.println("You got it right! Congratulation!!");//tell user if they are right
 			}
-			break;
+			else if(answer4 < firstnum / secondnum) //state if statement for if the user inputed an answer less than the correct answer
+			{
+				System.out.println("Try Again");//ask user to try again
+			}
+			else if(answer4 > firstnum / secondnum) //state if statement for if the user inputed an answer greater than the correct answer
+			{
+				System.out.println("Try Again");//ask user to try again
+			}
+			break;//break out of case
 			
 		}
 		
