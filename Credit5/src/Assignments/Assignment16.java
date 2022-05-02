17bc598438712c8463a43a1f2352647ef6aa28f9
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-05-02 13:10:30 -0600

    money

--- Credit5/src/Assignments/Assignment16.java
+++ Credit5/src/Assignments/Assignment16.java
@@ -23,19 +23,19 @@ public class Assignment16
 		
 		int secretnum = (int) (1 + (Math.random() * 20));//find secret random number between 1 - 20
 		
-		System.out.println("Enter a number between 1 and 20: ");
-		int userans = userinput.nextInt();
+		System.out.println("Enter a number between 1 and 20: ");//ask user to input a number 1 through 20
+		int userans = userinput.nextInt();//assign the number the user input to a variable called userans
 		
-		System.out.println("Computer's Number is: " + secretnum + "\n");
-		System.out.println("Player's Number is: " + userans + "\n");
+		System.out.println("Computer's Number is: " + secretnum + "\n");//print the number the computer picked from 1 - 20
+		System.out.println("Player's Number is: " + userans + "\n");//print the number the player picked from 1 - 20
 		
-		if(userans == secretnum)
+		if(userans == secretnum)//if statement for if the player number is equal to the computer number
 		{
-			System.out.println("You Won!");
+			System.out.println("You Won!");//print You Won! if the user was right
 		}
-		else 
+		else //else statement for if the player did not guess the computer number correctly
 		{
-			System.out.println("Better luck next time.");
+			System.out.println("Better luck next time.");//print Better luck next time. if the user was wrong
 		}
 	}
 }
