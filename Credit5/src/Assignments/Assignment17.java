92ab2353b17d197ee6a20492ea10bd7106a3541c
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-05-04 13:18:11 -0600

    money

--- Credit5/src/Assignments/Assignment17.java
+++ Credit5/src/Assignments/Assignment17.java
@@ -19,19 +19,34 @@ public class Assignment17
 	{
 		Scanner userinput = new Scanner(System.in);//create scanner object
 		
-		System.out.println("Enter a GPA or -1 to quit the program: ");
-		double gpa  = userinput.nextDouble();
+		System.out.println("Enter a GPA or -1 to quit the program: ");//ask user to enter GPA or enter -1 to end application
+		double gpa  = userinput.nextDouble();//assign user input to variable gpa
 		
-		while(gpa != -1) 
+		while(gpa != -1) //create while loop for when user does not enter -1
 		{
-			if(gpa >= 3.8)
+			if(gpa >= 3.8)//if statement for if the users GPA is greater than or equal to 3.8
 			{
-				System.out.println("summa cum laude");
-				System.out.println("Would you like to enter another GPA or quit: ");
-				double gpa1 = userinput.nextDouble();
+				System.out.println("summa cum laude\n");//print this if the if statement is true
+				
+			}
+			else if(gpa > 3.65 && gpa < 3.8) //create an else if statement for if the users input is greater than 3.65 and less than 3.8
+			{
+				System.out.println("magna cum laude\n");//print this if the else if statement is true
+			}
+			else if(gpa > 3.5 && gpa < 3.65) //create an else if statement for if the users input is greater than 3.5 and less than 3.65
+			{
+				System.out.println("cum laude\n");//print this if the else if statement is true
+			}
+			else //create else statement for if the users GPA doesn't meet the requirements
+			{
+				System.out.println("Sorry, at this you did not qualify for an honors distinction. \n");//tell the user they did not meet the requirements for the honors distinction
 			}
 			
-			
+			System.out.println("Would you like to enter another GPA or you can enter -1 to quit the application: ");//ask user if they would like to enter another GPA
+			 gpa = userinput.nextDouble();
 		}
+		
+		System.out.println("Thank you for using our program, have a great day!");//saying thank you to the user for using the application
+		
 	}
 }
