b957df7507b9c3073978b3fe902c5a31a26f6e84
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-05-05 15:00:47 -0600

    money

--- Credit5/src/Assignments/JavaFinalProject.java
+++ Credit5/src/Assignments/JavaFinalProject.java
@@ -19,15 +19,33 @@ public class JavaFinalProject
 	{
 		Scanner userinput = new Scanner(System.in);//create scanner object
 		
-		System.out.println("Enter 1 if you live in Alberta and would like to travel somewhere in Canada or enter 2 if you plan on travelling outside of Canada: ");
+		System.out.println("Enter 1 if you would like to travel to a province or enter 2 if you would like to travel to a territory (this application is only applicable if you live in Calgary): ");
 		int travelling = userinput.nextInt();
 		
 		switch (travelling) 
 		{
 		case 1:
 			System.out.println("Enter full name of the province you would you like to visit?: ");
-			int BritishColumbia = 255;
+			int province = userinput.nextInt();
 			
+			
+			int BritishColumbia = 255;
+			int Yukon = 675;
+			int NorthwestTerritory = 497;
+			int Saskatchewan = 406;
+			int Manitoba = 448;
+			int Ontario = 283;
+			int Quebec = 425;
+			int NewFoundland = 966;
+			int NewBrunswick = 1041;
+			int NovaScotia = 726;
+			int PrinceEdwardIsland = 649;
+			int Nunavaut = 370;
+			
+			if(province == British Columbia) 
+			{
+				System.out.println("The fligth cost to travel to British Columbia is $255.");
+			}
 			
 			
 			
@@ -35,6 +53,6 @@ public class JavaFinalProject
 			
 			
 		}
-		System.out.println("Sorry, this application is only designed for travelling inside Canada.");
+		
 	}
 }
