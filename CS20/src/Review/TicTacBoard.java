feb76780f74fed578ca570efec68133997bb7144
46075001 <46075001@L815WD220015.edu.cbe.ab.ca>
2022-09-07 13:45:57 -0600

    money

--- CS20/src/Review/TicTacBoard.java
+++ CS20/src/Review/TicTacBoard.java
@@ -1,5 +1,29 @@
 package Review;
+/*
 
-public class TicTacBoard {
+Program: TicTacBoard.java          Date: September 7, 2022
 
+
+Author: Money Mann 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+public class TicTacBoard 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("       |        |");
+		System.out.println("       |        |");
+		System.out.println("       |        |");
+		System.out.println("------------------------");
+		System.out.println("       |        |");
+		System.out.println("       |   x    |");
+		System.out.println("       |        |");
+		System.out.println("------------------------");
+		System.out.println("       |        |");
+		System.out.println("       |        |");
+		
+	}
 }
